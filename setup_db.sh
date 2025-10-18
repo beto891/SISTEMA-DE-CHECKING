c567d5fd98c427817e37c7fb6b79195f581542b5
Ediberto <edibertosantana18@gmail.com>
2025-10-17 22:03:20 -0300

    correção de bash

--- setup_db.sh
+++ setup_db.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Este script inicializa o banco de dados e cria o admin antes de iniciar o servidor.
 
-# 1. Executa o script de inicialização do banco de dados (Usando a CLI do Flask, que é mais estável)
-# O comando 'flask' encontra e executa a função inicializar_banco no contexto da aplicação.
-python -c 'from app import create_app; app = create_app(); app.app_context().push(); from app.utils.database import inicializar_banco; inicializar_banco(app); app.app_context().pop()'
+# 1. Executa o script de inicialização do banco de dados (Corrige o IndexError removendo o pop())
+python -c 'from app import create_app; app = create_app(); app.app_context().push(); from app.utils.database import inicializar_banco; inicializar_banco(app)'
 
 # 2. Inicia o servidor Gunicorn com o worker assíncrono para SocketIO
-# (O Start Command do Render deve ser APENAS: ./setup_db.sh)
 gunicorn run:app -k gevent -b 0.0.0.0:$PORT
