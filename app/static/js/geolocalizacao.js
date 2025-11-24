5f560ee696f2e1b1e23a928b122f69e379530931
Ediberto <edibertosantana18@gmail.com>
2025-11-23 23:56:52 -0300

    Ajuste de tamanho icone GPS

--- app/static/js/geolocalizacao.js
+++ app/static/js/geolocalizacao.js
@@ -39,7 +39,7 @@ function successLocation(pos) {
     const userIcon = L.divIcon({
         className: 'user-location-marker',
         iconSize: [20, 20],
-        iconAnchor: [10, 10]
+        iconAnchor: [50, 50]
     });
 
     // Se o marcador não existe, cria
@@ -56,7 +56,7 @@ function successLocation(pos) {
         }).addTo(map);
 
         // Opcional: Centraliza a câmera no usuário na primeira vez que acha
-        // map.setView([lat, lng], 16); 
+         map.setView([lat, lng], 16); 
     } else {
         // Se já existe, só move (para não ficar piscando)
         userMarker.setLatLng([lat, lng]);
